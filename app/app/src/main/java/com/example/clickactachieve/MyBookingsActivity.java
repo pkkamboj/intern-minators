ae5f16860bb5e22f1fe5a81922e3db08450cb027
wangsh <shirley.wang@pointclickcare.com>
2019-10-23 16:32:26 -0400

    partial code for booking page

--- app/app/src/main/java/com/example/clickactachieve/MyBookingsActivity.java
+++ app/app/src/main/java/com/example/clickactachieve/MyBookingsActivity.java
@@ -1,15 +1,38 @@
 package com.example.clickactachieve;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MyBookingsActivity extends AppCompatActivity {
 
+    Button bookActivity;
+    Button existingBookings;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_bookings);
+
+        bookActivity = findViewById(R.id.bookActivityButton);
+        existingBookings = findViewById(R.id.viewBookingsButton);
+
+        bookActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        existingBookings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openNewAccountPage();
+            }
+        });
+
+
     }
 }
