8df0b9dc6245d91144600505c3d1f3af1f2a8704
Nikhil Shende <nkhlshnd99@gmail.com>
2019-10-23 10:34:52 -0400

    Buttons

--- app/app/src/main/java/com/example/clickactachieve/MainActivity.java
+++ app/app/src/main/java/com/example/clickactachieve/MainActivity.java
@@ -2,12 +2,32 @@ package com.example.clickactachieve;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    final Button BUTTON_LOGIN = findViewById(R.id.);
+    final Button BUTTON_CREATE_ACCOUNT = findViewById(R.id.);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        BUTTON_LOGIN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        BUTTON_CREATE_ACCOUNT.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
