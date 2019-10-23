9eca1928895cff9081e01debb2e9537f7ffc27c6
Nikhil Shende <nkhlshnd99@gmail.com>
2019-10-23 14:17:03 -0400

    Intent to access UserLandingActivity

--- app/app/src/main/java/com/example/clickactachieve/LoginActivity.java
+++ app/app/src/main/java/com/example/clickactachieve/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.example.clickactachieve;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -21,6 +22,7 @@ public class LoginActivity extends AppCompatActivity {
     Button loginButton = findViewById(R.id.button_login);
     TextView responseText = findViewById(R.id.text_error);
     RequestQueue queue = Volley.newRequestQueue(this);
+    Intent userLandingIntent = new Intent(this, UserLandingActivity.class);
 
     private void login(){
         String url = "localhost:3000/api/auth";
@@ -47,8 +49,9 @@ public class LoginActivity extends AppCompatActivity {
             public void onClick(View v) {
                 loginButton.setEnabled(false);
                 //Make API call to localhost:3000/api/auth/
-                login();
+                //login();
                 loginButton.setEnabled(true);
+                startActivity(userLandingIntent);
             }
         });
     }
