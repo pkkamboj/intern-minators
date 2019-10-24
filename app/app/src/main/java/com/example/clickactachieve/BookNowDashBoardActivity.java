89c7f86736804d6f578cbfbf7f14f58dc5774859
jterryhao <44141680+jterryhao@users.noreply.github.com>
2019-10-24 10:34:09 -0400

    add btns to dashboard

--- app/app/src/main/java/com/example/clickactachieve/BookNowDashBoardActivity.java
+++ app/app/src/main/java/com/example/clickactachieve/BookNowDashBoardActivity.java
@@ -1,6 +1,8 @@
 package com.example.clickactachieve;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,5 +12,10 @@ public class BookNowDashBoardActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_booknow_dashboard);
+    }
+
+    public void onPhyscialBtnClicked(View v) {
+        Intent intent = new Intent(this, PickDateActivity.class);
+        startActivity(intent);
     }
 }
