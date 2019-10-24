1adc534b409f6ab9aa765dc7cb1339bc67a274cd
wangsh <shirley.wang@pointclickcare.com>
2019-10-24 09:49:32 -0400

    changing userLanding

--- app/app/src/main/java/com/example/clickactachieve/UserLandingActivity.java
+++ app/app/src/main/java/com/example/clickactachieve/UserLandingActivity.java
@@ -3,23 +3,45 @@ package com.example.clickactachieve;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class UserLandingActivity extends AppCompatActivity {
+public class MyBookingsActivity extends AppCompatActivity {
+
+    Button bookActivity;
+    Button existingBookings;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_user_landing);
+        setContentView(R.layout.activity_my_bookings);
+
+        bookActivity = findViewById(R.id.bookActivityButton);
+        existingBookings = findViewById(R.id.viewBookingsButton);
+
+        bookActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                clickBookActivity();
+            }
+        });
+
+        existingBookings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                clickMyBookings();
+            }
+        });
     }
 
-    public void onBookNowClicked(View v) {
+    public void clickBookActivity() {
         Intent intent = new Intent(this, BookNowDashBoardActivity.class);
         startActivity(intent);
     }
 
-    public void onMyBookingsClicked(View v) {
+    public void clickMyBookings() {
         Intent intent = new Intent(this, MyBookingsActivity.class);
         startActivity(intent);
     }
