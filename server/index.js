797787b2e7a7986b3e966552827586d0bd336d58
Joseph Hu <hujoseph99@gmail.com>
2019-10-24 09:43:39 -0400

    testing db connection

--- server/index.js
+++ server/index.js
@@ -1,11 +1,21 @@
 const express = require("express");
 const config = require("config");
 const app = express();
+const mongoose = require("mongoose");
 
 app.use(express.json());
 
 // DB Config
 const dbUri = config.get("mongoURI");
+
+mongoose
+  .connect(dbUri, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log("MongoDB connected... "))
+  .catch(err => console.log(err));
 
 // Routes
 app.use("/api/auth", require("./routes/api/auth"));
