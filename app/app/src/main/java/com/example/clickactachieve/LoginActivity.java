ceb6e0f90172b72ae5dfb942ba0efa80049d38c4
Jia Hao <j28hao@uwaterloo.ca>
2019-10-24 22:21:28 -0400

    add styling for some files

--- app/app/src/main/java/com/example/clickactachieve/LoginActivity.java
+++ app/app/src/main/java/com/example/clickactachieve/LoginActivity.java
@@ -2,32 +2,16 @@ package com.example.clickactachieve;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-
-import java.io.BufferedOutputStream;
-import java.io.IOException;
-import java.io.OutputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.HashMap;
-import java.util.Map;
-
-import okhttp3.FormBody;
-import okhttp3.MediaType;
-import okhttp3.MultipartBody;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.RequestBody;
-import okhttp3.Response;
-import okio.BufferedSink;
 
 
 public class LoginActivity extends AppCompatActivity {
@@ -38,75 +22,21 @@ public class LoginActivity extends AppCompatActivity {
     final String TAG = "LoginActivity123";
 
     private void login() {
-        if (usernameText.getText().toString().equals("username") && passwordText.getText().toString().equals("password")){
+        if (usernameText.getText().toString().equals("pcciamsuperuser") && passwordText.getText().toString().equals("password")){
             System.out.println("hi");
             openUserLanding();
         } else if (usernameText.getText().toString().equals("facility") && passwordText.getText().toString().equals("password")) {
             Log.d(TAG, "login: test");
             openFacilityLanding();
+        } else {
+            Toast toast = Toast.makeText(getApplicationContext(), "Your username or password is incorrect!", Toast.LENGTH_LONG);
+            toast.show();
         }
-
-        /*URL apiUrl = new URL("http://10.0.2.2:3000/api/auth/login") ;
-        HttpURLConnection urlConnection = (HttpURLConnection) apiUrl.openConnection();
-
-        try {
-            urlConnection.setDoOutput(true);
-            urlConnection.setChunkedStreamingMode(0);
-
-            OutputStream out = new BufferedOutputStream(urlConnection.getOutputStream());
-
-        }
-        Map<String,String> map = new HashMap<String, String>();
-        map.put("username", usernameText.getText().toString());
-        map.put("password", passwordText.getText().toString());
-
-        ObjectMapper objectMapper = new ObjectMapper();
-        String requestBody = objectMapper
-                .writerWithDefaultPrettyPrinter()
-                .writeValueAsString(map);
-
-        HttpRequest request = HttpRequest.newBuilder(apiUrl)
-                .header("Content-Type", "application/json")
-                .POST(BodyPublishers.ofString(requestBody))
-                .build();
-
-        HttpClient.newHttpClient()
-                .sendAsync(request, BodyHandlers.ofString())
-                .thenApply(HttpResponse::statusCode)
-                .thenAccept(System.out::println);*/
-
-        /*Log.d(TAG, "1");
-        String apiUrl = "http://10.0.2.2:3000/api/auth/login";
-        Log.d(TAG, "2");
-        RequestBody requestBody
-        Log.d(TAG, "3");
-        final Request loginRequest = new Request.Builder()
-                .url(apiUrl)
-                .post(requestBody)
-                .header("Content-Type","application/json")
-                .build();
-        Log.d(TAG, "4");
-        Runnable r = new Runnable() {
-            public void run() {
-                try {
-                    Log.d(TAG, "6");
-                    Response response = new OkHttpClient().newCall(loginRequest).execute();
-                    Log.d(TAG, response.message() + response.toString());
-                    responseText.setText(response.message());
-                    Log.d(TAG, "7");
-                }
-                catch (Exception e){
-                    Log.d(TAG,  e.toString());
-                }
-            }
-        };
-        Thread thread = new Thread(r);
-        thread.start();
-        Log.d(TAG, "5");*/
     }
 
     private void openUserLanding(){
         Intent intent = new Intent(this, UserLandingActivity.class);
+        intent.putExtra("username", "pcciamsuperuser");
         startActivity(intent);
     }
 
@@ -132,5 +62,9 @@ public class LoginActivity extends AppCompatActivity {
                 loginButton.setEnabled(true);
             }
         });
+    }
+
+    public void onBackBtnClicked(View v) {
+        finish();
     }
 }
--- app/app/src/main/java/com/example/clickactachieve/UserLandingActivity.java
+++ app/app/src/main/java/com/example/clickactachieve/UserLandingActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,7 +14,13 @@ public class UserLandingActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Intent intent = getIntent();
+        String username = intent.getStringExtra("username");
+
         setContentView(R.layout.activity_user_landing);
+        TextView usernameView = (TextView) findViewById(R.id.hello_usern);
+        usernameView.setText("Hello, \n".concat(username));
     }
 
     public void clickBookActivity(View v) {
